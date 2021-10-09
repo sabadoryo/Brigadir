f836d28b88f97a7b66f6158ebbbc8ffc255b8c4c
Yernar Abilbay <eabilbay@gmail.com>
2021-10-09 17:47:18 +0300

    scheduler.js

--- index.js
+++ index.js
@@ -17,29 +17,6 @@ const client = new Client({
     partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
 });
 
-const schedule = require('node-schedule');
-
-var rule = new schedule.RecurrenceRule();
-
-rule.hours = 3;
-
-schedule.scheduleJob(rule, async function () {
-    let image = "";
-    let caption = "";
-
-    await axios.get(`https://some-random-api.ml/meme`)
-        .then(function (response) {
-            console.log(response);
-            image = response.data.image;
-            caption = response.data.caption
-        })
-        .catch(function (error) {
-            console.log(error)
-        })
-
-    const channel = await client.channels.fetch('894551552281444363');
-    channel.send({files: [`${image}`], content: caption});
-});
 
 client.on('ready', () => {
     console.log(client.user.tag)
